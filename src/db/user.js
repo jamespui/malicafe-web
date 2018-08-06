20ef778a8a5e6ea8e5325d05235a9d54b4b5bcb6
LaoHan <jimocheng97@hotmail.com>
2018-08-06 21:40:18 +0800

    Merge remote-tracking branch 'origin/master' into laohanme
    # Conflicts:
    #	package-lock.json

---
+++ src/db/user.js
@@ -0,0 +1,12 @@
+const {User} = require('../models');
+
+const db = require('./../models');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+export class {
+
+    static create(data, cb) {
+
+    }
+
+}
