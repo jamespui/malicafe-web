006aae41103f94315e454b65d6c6a8eeff9ec484
LaoHan <jimocheng97@hotmail.com>
2018-08-12 01:36:29 +0800

    feat make login register and profile work session

--- src/app.js
+++ src/app.js
@@ -1,32 +1,58 @@
-const express = require('express');
-const favicon = require('serve-favicon');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-const routing = require('./routes');
+let express = require('express');
+let app = express();
+let favicon = require('serve-favicon');
+let path = require('path');
+let mongoose = require('mongoose');
+let passport = require('passport');
+let flash = require('connect-flash');
 
+let morgan = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let session = require('express-session');
+
+let routing = require('./routes/index');
+let configDB = require('./config/database');
+
+// configuration ===============================================================
 mongoose.promise = global.Promise;
+mongoose.connect(configDB.url, {useNewUrlParser: true}); // connect to our database
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+    console.log('Malicafe Database is Connected');
+});
 
-const app = express();
+require('../src/config/passport')(passport); // pass passport for configuration
 
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({extended: true}));
+
+// file
 // View filetype .pug
 app.set('view engine', 'pug');
-
 // Templates
 app.set('views', path.join(__dirname, '..', 'views'));
-
 // Static
 app.use('/static', express.static(path.join(__dirname, '..', 'static')));
 
-// Middleware to parse POST request & cookies
-// Must include if you want to parse POST request
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+// session
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 // Routing link
 app.use(routing);
+require('./routes/user_route.js')(app, passport);
 
 app.use(favicon(path.join(__dirname, '..', 'static', 'favicon', 'favicon.png')));
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,31 +1,14 @@
 const express = require('express');
 const app = new express.Router();
-const mongoose = require('mongoose');
 const apiURL = require('./api');
 
-mongoose.connect('mongodb://localhost:27017/malicafeDB?authSource=admin', {useNewUrlParser: true});
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    // we're connected!
-    console.log('Malicafe Database is Connected');
-});
 
 // API CALL LINK
 app.use('/api', apiURL);
 
-
 // Serve link example (http://localhost:3018/__)
 app.get('/', function(req, res) {
     res.render('home', {title: 'Malicafe'});
-});
-
-app.get('/login', function(req, res) {
-    res.render('account/login', {title: 'Login'});
-});
-
-app.get('/register', function(req, res) {
-    res.render('account/register', {title: 'Register'});
 });
 
 app.get('/resetpassword', function(req, res) {
