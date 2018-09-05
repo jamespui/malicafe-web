98c56d2e0fb568763a07a62cb22255d7a56e2332
LaoHan <jimocheng97@hotmail.com>
2018-09-05 22:07:45 +0800

    update: now admin can view user register data

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,35 +1,37 @@
 const express = require('express');
 const app = new express.Router();
+let mongoose = require('mongoose');
 // import User from '../../helpers/db/user';
-const UserModel = require('../../models/user');
+// const UserModel = require('../../models/user');
 
 
-app.post('/register', function(req, res) {
-    const user = req.body;
-    let newUser = new UserModel();
-    let password = newUser.generateHash(
-        user.password
-    );
-    newUser.data = {
-        userName: user.userName,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        password: password
-    };
-    newUser.save();
-    res.json({
-        success: true,
-        msg: 'Your Account Created'
+// app.post('/register', function(req, res) {
+//     const user = req.body;
+//     let newUser = new UserModel();
+//     let password = newUser.generateHash(
+//         user.password
+//     );
+//     newUser.data = {
+//         userName: user.userName,
+//         firstName: user.firstName,
+//         lastName: user.lastName,
+//         email: user.email,
+//         password: password
+//     };
+//     newUser.save();
+//     res.json({
+//         success: true,
+//         msg: 'Your Account Created'
+//     });
+//     console.log(newUser.data);
+// });
+
+app.get('/user/read/all', function(req, res) {
+    let userdata = mongoose.model('User');
+    userdata.find(function(err, response) {
+        if (err) return console.error(err);
+        res.json(response);
     });
-    console.log(newUser.data);
-});
-
-app.get('/read', function(req, res) {
-    let data = {
-        name: 'hello'
-    };
-    res.json(data);
 });
 
 module.exports = app;
