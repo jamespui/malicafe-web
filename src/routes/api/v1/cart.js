381c544fa8d024fe9ec082087baeaa2868d10d1e
LaoHan <jimocheng97@hotmail.com>
2018-09-22 09:38:22 +0800

    chore: remove unuse code

--- src/routes/api/v1/cart.js
+++ src/routes/api/v1/cart.js
@@ -5,26 +5,9 @@ const cartModel = require('../../../models/cart');
 
 // add to cart
 app.post('/cart/add', function(req, res) {
-
-    let cartObj = req.body;
-    cartObj.userID = id;
-    let cartObj = new cartModel(data);
-    // cb(err, data);
-    cartObj.save(function(err, obj) {
-        cb(err, obj);
-    });
-
     res.json({
         success: true,
         msg: 'Add Success',
-        data: addtocart
+        data: 'Hello'
     });
 });
-
-static create(data, cb) {
-    let cartObj = new db.Cart(data);
-    // cb(err, data);
-    cartObj.save(function(err, obj) {
-        cb(err, obj);
-    });
-}
